f82cda9b3b0dec68e123e6ca3cbd81cd6cc8a1c6
Draycia <draycia98@gmail.com>
2017-12-17 04:10:01 -0800

    Fixed formatting and added Versions endpoint

--- app.js
+++ app.js
@@ -1,65 +1,74 @@
 var express = require('express');
+var stylus = require('stylus')
+var nib = require('nib')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stylus = require('stylus');
-var nib = require('nib');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+var compression = require('compression');
+var minify = require('express-minify');
+var request = require('request');
 
 var publicDir = path.join(__dirname, 'public');
+var date = new Date();
+var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 function compile(str, path) {
   return stylus(str)
-      .set('filename', path)
-      .use(nib())
+    .set('filename', path)
+    .use(nib())
 }
 
 app.use(stylus.middleware({
   src: __dirname + '/public',
   compile: compile
-}));
+}))
 
-app.use(favicon(path.join(publicDir, 'bronze.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'bronze.ico')));
+app.use(compression());
+app.use(logger('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(publicDir));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('*', function(req, res, next) {
+//     if (!req.secure) {
+//         return res.redirect('https://' + req.get('host') + req.originalUrl);
+//     }
+//     return next();
+// });
 
 app.get('/', (req, res) => {
+  getVersion();
   if (req.query.summonerName) {
-    res.render('summoner', {summonerName: req.query.summonerName});
+    res.render('summoner', { summonerName: req.query.summonerName });
   } else {
     res.render('index');
   }
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.get('/api/versions/ddragon', (req, res) => {
+  request.get('http://ddragon.leagueoflegends.com/api/versions.json', (err, response, body) => {
+    res.send(JSON.parse(body)[0]);
+  });
+});
+
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = app;
+module.exports = app; y
