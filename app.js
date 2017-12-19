23709d4fc52d888e532f13ec1c2e087703441590
Draycia <draycia98@gmail.com>
2017-12-18 18:44:44 -0800

    Merged local with upstream

--- app.js
+++ app.js
@@ -43,10 +43,68 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     return next();
 // });
 
+userData = {
+  summonerInfo: { name: 'Rito Bandito', iconId: 3186 },
+  match:
+    {
+      perks:
+        {
+          perk0Id: 8229,
+          perk1Id: 8226,
+          perk2Id: 8234,
+          perk3Id: 8237,
+          perk4Id: 9104,
+          perk5Id: 8014
+        },
+      queueId: 400,
+      mapId: 11,
+      championId: 202,
+      spell1Id: 7,
+      spell2Id: 4,
+      kills: 5,
+      deaths: 7,
+      assists: 8,
+      totalDamageDealt: 52337,
+      goldEarned: 9058,
+      gameLength: 1502,
+      wardsPlaced: 4,
+      pinksPlaced: 2
+    }
+}
+
+proData = {
+  summonerInfo: { name: 'Imaqtpie', iconId: 7 },
+  match:
+    {
+      perks:
+        {
+          perk0Id: 8229,
+          perk1Id: 8226,
+          perk2Id: 8234,
+          perk3Id: 8237,
+          perk4Id: 9104,
+          perk5Id: 8014
+        },
+      queueId: 400,
+      mapId: 11,
+      championId: 202,
+      spell1Id: 7,
+      spell2Id: 4,
+      kills: 5,
+      deaths: 7,
+      assists: 8,
+      totalDamageDealt: 52337,
+      goldEarned: 17783,
+      gameLength: 2100,
+      wardsPlaced: 4,
+      pinksPlaced: 2
+    }
+}
+
 app.get('/', (req, res) => {
   if (req.query.summonerName) {
     request.get('http://ddragon.leagueoflegends.com/api/versions.json', (err, response, body) => {
-      res.render('summoner', { summonerName: req.query.summonerName, verion: JSON.parse(body)[0], wardCount: 8, pinkCount: 2 });
+      res.render('summoner', { version: JSON.parse(body)[0], userData: userData, proData: proData });
     });
   } else {
     res.render('index');
